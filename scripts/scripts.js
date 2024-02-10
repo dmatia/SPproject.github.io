3209d7c9a3d3a20a59213972568bdd00047b8f67
dmatia <diegovillalba.96@gmail.com>
2024-02-10 00:29:17 -0300

    menu

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,14 @@
 function openMenu(){
-    
+    document.getElementById("menu").style.cssText = 'margin-left: 0';
+    hideContent();
+}
+
+function closeMenu(){
+    document.getElementById("menu").style.cssText = 'margin-left: -100%';
+    hideContent();
+}
+
+function hideContent(){
+    var contenido = document.getElementById("contenedor");
+    contenido.classList.toggle("hideContent");
 }
